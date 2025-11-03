4c264ec840d51d44059bff73451504f5a080580d
mikitavydrankou <mikitavydrankou@gmail.com>
2025-11-03 19:09:31 +0100

    testing migtations

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, blank=True)
+    body = models.TextField()
+    published_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
